25592690911b7d85998b39f2c85bfda7fb87e5c2
qufaoo <quafoochan@gmail.com>
2019-06-07 20:30:11 +1000

    feat: process response headers

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,4 +1,5 @@
 import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types/index'
+import { parseHeades } from '../src/helpers/headers'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise(resolve => {
@@ -17,7 +18,7 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         return
       }
 
-      const responseHeaders = request.getAllResponseHeaders()
+      const responseHeaders = parseHeades(request.getAllResponseHeaders())
       const responseData =
         responseType && responseType !== 'text' ? request.response : request.responseText
       const response: AxiosResponse = {
